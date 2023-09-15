6d051bbdd3fb5c234c6a362b5ce1c0a84ffcaddd
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-09-15 18:01:59 -0500

    Replace Python with JavaScript

--- states.js
+++ states.js
@@ -1,3 +1,14 @@
-function step(state, input, start) {}
+function step(state, input, graph) {
+    return graph[state][input]
+}
 
-function run() {}
+function run(state, inputs, graph) {
+    visited = [state]
+
+    for (const input of inputs) {
+        state = step(state, input, graph)
+        visited.push(state)
+    }
+
+    return visited
+}
