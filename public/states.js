0e6002a2f5d6138bba66a0b62ad6e6afeecb30e6
Ad31ide <seanchendu@gmail.com>
2023-09-15 18:50:17 -0500

    setup it

---
+++ public/states.js
@@ -0,0 +1,45 @@
+
+
+// create an array with nodes
+var nodes = new vis.DataSet([
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" },
+]);
+
+// create an array with edges
+var edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 },
+]);
+
+// create a network
+var container = document.getElementById("mynetwork");
+var data = {
+  nodes: nodes,
+  edges: edges,
+};
+var options = {};
+var network = new vis.Network(container, data, options);
+
+
+function step(state, input, graph) {
+    return graph[state][input]
+} 
+
+
+function run(state, inputs, graph) {
+    visited = [state]
+
+    for (const input of inputs) {
+        state = step(state, input, graph)
+        visited.push(state)
+    }
+
+    return visited
+}
